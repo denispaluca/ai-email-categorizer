2f2e3e0dbfeaccb773bd24912d371a0904ecadce
Denis Paluca <denispaluca@gmail.com>
2026-01-11 21:11:32 +0100

    dummy commit

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import * as schema from "./schema";
 
-const sqlite = new Database(process.env.DB_PATH!);
-export const db = drizzle(sqlite, { schema });
+const sqliteDb = new Database(process.env.DB_PATH!);
+export const db = drizzle(sqliteDb, { schema });
 
 export type Database = typeof db;
